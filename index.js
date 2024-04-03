883e11d9458181b6da8818d493ea3a1fd84d5b40
Sonupr130 <sonupradhan130@gmail.com>
2024-04-03 20:39:35 +0530

    Create post, like, comment modal

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ require('dotenv').config();
 const PORT = process.env.PORT || 5000;
 
 //middleware
-// app.use(express.json());
+app.use(express.json());
 
-// const blog = require('./routes/blog');
+const blog = require('./routes/blog');
 
 // mount
-// app.use('/api/v1', blog);
+app.use('/api/v1', blog);
 
 const connectwithDb = require('./config/database');
 connectwithDb();
