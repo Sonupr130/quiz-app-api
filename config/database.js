67a013b5db3a331897a92bdd14a7fade58b6f273
Sonupr130 <sonupradhan130@gmail.com>
2024-04-03 20:02:12 +0530

    establish db connection with app and basic setup

---
+++ config/database.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const connectwithDb = () => {
+    mongoose.connect(process.env.DATABASE_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(console.log("DB Connected Successfully"))
+    .catch((err) => {
+        console.log("DB faciing connection issues");
+        console.log(err);
+        process.exit(1);
+    })
+    
+};
+
+module.exports = connectwithDb;
---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+
+require('dotenv').config();
+const PORT = process.env.PORT || 5000;
+
+//middleware
+// app.use(express.json());
+
+// const blog = require('./routes/blog');
+
+// mount
+// app.use('/api/v1', blog);
+
+const connectwithDb = require('./config/database');
+connectwithDb();
+
+// start the server
+app.listen(PORT, () => {
+    console.log("Server is Started!")
+})
+
+app.get("/", (req, res) => {
+    req.send('<h1>This is homepage<h1/>')
+})
